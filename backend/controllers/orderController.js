fe0595a985fe6a4b173e68e6e618367394998e70
allan <allansaleh95@gmail.com>
2023-12-31 00:11:14 -0500

    updateOrderToDelivered controller function

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -94,7 +94,19 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 // @route   PUT /api/orders/:id/deliver
 // @access  Private/Admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
-  res.send('update order to delivered');
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+
+    const updatedOrder = await order.save();
+
+    req.status(200).json(updatedOrder);
+  } else {
+    res.status(400);
+    throw new Error('Order not found');
+  }
 });
 
 // @desc    Get all orders
