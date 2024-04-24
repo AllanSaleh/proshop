577bdb983f0ddd2207ee3fa372eff63b187215f5
allan <allansaleh95@gmail.com>
2024-04-24 03:23:54 -0400

    fixed a bug in the checkObjectId middleware

--- backend/middleware/checkObjectId.js
+++ backend/middleware/checkObjectId.js
@@ -1,7 +1,7 @@
 import { isValidObjectId } from 'mongoose';
 
 function checkObjectId(req, res, next) {
-  if (!isValidObjectId(req, res, next)) {
+  if (!isValidObjectId(req.params.id)) {
     res.status(404);
     throw new Error(`Invalid ObjectId of: ${req.params.id}`);
   }
